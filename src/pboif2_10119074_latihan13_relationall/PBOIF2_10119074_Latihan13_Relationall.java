3234b30acec6536eea7fe659db0f9e925db8e508
HandrianRivaldi74 <handrianr28@gmail.com>
2020-10-18 15:47:44 +0700

    Latihan13

---
+++ src/pboif2_10119074_latihan13_relationall/PBOIF2_10119074_Latihan13_Relationall.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10119074_latihan13_relationall;
+
+/**
+ *
+ * @author Acer
+ * NAMA     : Handrian Rivaldi
+ * KELAS    : PBOIF2
+ * NIM      : 10119074
+ * Deskrifsi : Relational
+ */
+public class PBOIF2_10119074_Latihan13_Relationall {
+
+    public static void main(String[] args) {
+        int bi11 = 10;
+        int bi12 = 20;
+        System.out.println("a == b = " + (bi11 == bi12));
+        System.out.println("a != b = " + (bi11 != bi12));
+        System.out.println("a > b = " + (bi11 > bi12));
+        System.out.println("a < b = " + (bi11 < bi12));
+        System.out.println("b >= a = " + (bi12 >= bi11));
+        System.out.println("b <= a = " + (bi12 <= bi11));
+    }
+    
+}
